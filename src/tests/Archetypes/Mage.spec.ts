1c487d34d9cc24d75fdc6b8eb26749981e63c5a5
Renato Mendes <mendeslrenato@gmail.com>
2022-11-18 19:52:00 -0300

    test: ensure Mage class returns the right name when getter is called

---
+++ src/tests/Archetypes/Mage.spec.ts
@@ -0,0 +1,14 @@
+import Archetype, { Mage } from "../../Archetypes"
+
+const makeSut = (): Archetype => {
+  const name = 'Gandalf';
+  const energyType = 'mana';
+  return new Mage(name, energyType);
+}
+
+describe('Mage Class', () => {
+  it('Should return the right name when getter is called', () => {
+    const sut = makeSut()
+    expect(sut.name).toBe('Gandalf');
+  });
+})
