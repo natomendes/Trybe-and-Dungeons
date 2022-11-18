c2f9cfd0c78363293cdc6eed6ac62cfe6d97447c
Renato Mendes <mendeslrenato@gmail.com>
2022-11-18 20:13:32 -0300

    feat: create class Necromancer

---
+++ src/tests/Archetypes/Necromancer.spec.ts
@@ -0,0 +1,43 @@
+import Archetype, { Necromancer } from "../../Archetypes"
+
+const makeSut = (): Archetype => {
+  const name = 'Sauron';
+  const energyType = 'mana';
+  return new Necromancer(name);
+}
+
+const makeSutWithSpecialValue = (): Archetype => {
+  const name = 'Sauron';
+  const energyType = 'mana';
+  const special  = 100;
+  return new Necromancer(name, special);
+}
+
+const makeSutWithCostValue = (): Archetype => {
+  const name = 'Sauron';
+  const energyType = 'mana';
+  const cost  = 70;
+  return new Necromancer(name, undefined, cost);
+}
+
+describe('Necromancer Class', () => {
+  it('Should return the right name when getter is called', () => {
+    const sut = makeSut()
+    expect(sut.name).toBe('Sauron');
+  });
+
+  it('Should have mana as energyType', () => {
+    const sut = makeSut()
+    expect(sut.energyType).toBe('mana');
+  });
+
+  it('Should return the right special when value is provided to constructor', () => {
+    const sut = makeSutWithSpecialValue()
+    expect(sut.special).toBe(100);
+  });
+
+  it('Should return the right cost when value is provided to constructor', () => {
+    const sut = makeSutWithCostValue()
+    expect(sut.cost).toBe(70);
+  });
+})
