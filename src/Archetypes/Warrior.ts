72f5e4237edd6e520a3bf5570e6a5de925c41ba9
Renato Mendes <mendeslrenato@gmail.com>
2022-11-18 20:24:09 -0300

    feat: create class Warrior

---
+++ src/Archetypes/Warrior.ts
@@ -0,0 +1,20 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Warrior extends Archetype {
+  private static createdInstances = 0;
+  private readonly _energyType: EnergyType = 'stamina';
+  constructor(
+    name: string,
+    special?: number,
+    cost?: number,
+  ) {
+    super(name, special || 0, cost || 0);
+
+    Warrior.createdInstances += 1;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
