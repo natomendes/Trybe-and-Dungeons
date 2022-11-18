f496c6a12cc020034a21b69c82c76ef6dc300d9d
Renato Mendes <mendeslrenato@gmail.com>
2022-11-18 20:20:24 -0300

    feat: create class Ranger

--- src/Archetypes/index.ts
+++ src/Archetypes/index.ts
@@ -1,10 +1,12 @@
 import Archetype from './Archetype';
 import Mage from './Mage';
 import Necromancer from './Necromancer';
+import Ranger from './Ranger';
 
 export default Archetype;
 
 export {
   Mage,
   Necromancer,
+  Ranger,
 };
