93aea5e0c24c6946a64cf0bbad11878b128c68bb
Renato Mendes <mendeslrenato@gmail.com>
2022-11-18 17:18:53 -0300

    test: ensure Dwarf returns the right name when getter name is called

--- src/tests/Dwarf.spec.ts
+++ src/tests/Dwarf.spec.ts
@@ -1,27 +1,22 @@
 import Race from "../Races";
+import Dwarf from "../Races/Dwarf";
 
 const makeSut = (): Race => {
   const name = 'Thorin';
   const dexterity = 50;
   const maxLifePoints = 80;
-  class DwarfStub extends Race {
-    constructor(
-      _name: string,
-      _dexterity: number,
-      private readonly _maxLifePoints: number,
-    ) {
-      super(_name, _dexterity);
-    }
-    get maxLifePoints(): number {
-      return this._maxLifePoints;
-    }
-  }
-  return new DwarfStub(name, dexterity, maxLifePoints);
+
+  return new Dwarf(name, dexterity, maxLifePoints);
 }
 
 describe('Dwarf Class', () => {  
   it('Should have attribute maxLifePoints', () => {
     const sut = makeSut();
-    expect(sut.maxLifePoints).toBeDefined()
-  })
+    expect(sut.maxLifePoints).toBeDefined();
+  });
+
+  it('Should return Thorin when name getter is called', () => {
+    const sut = makeSut();
+    expect(sut.name).toBe('Thorin');
+  });
 })
