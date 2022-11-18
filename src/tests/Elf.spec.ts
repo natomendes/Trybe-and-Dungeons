9b724a33d9add35f023c14ecbc81035b5537eb5b
Renato Mendes <mendeslrenato@gmail.com>
2022-11-18 18:27:12 -0300

    test: create Elf class

---
+++ src/tests/Elf.spec.ts
@@ -0,0 +1,25 @@
+import Race, { Elf } from "../Races";
+
+const makeSut = (): Race => {
+  const name = 'Legolas';
+  const dexterity = 80;
+
+  return new Elf(name, dexterity);
+}
+
+describe('Dwarf Class', () => {  
+  it('Should have maxLifePoints equal to 99', () => {
+    const sut = makeSut();
+    expect(sut.maxLifePoints).toBe(99)
+  });
+
+  it('Should return the right name when getter is called', () => {
+    const sut = makeSut();
+    expect(sut.name).toBe('Legolas');
+  });
+
+  it('Should return the right dexterity when getter is called', () => {
+    const sut = makeSut();
+    expect(sut.dexterity).toBe(80);
+  });
+})
