5a64466dcb25a3e4ac7da7478841b7109fddb4bf
Renato Mendes <mendeslrenato@gmail.com>
2022-11-18 18:24:52 -0300

    refactor: change Dwarf implementation

--- src/Races/Dwarf.ts
+++ src/Races/Dwarf.ts
@@ -1,12 +1,20 @@
 import Race from './Race';
 
 export default class Dwarf extends Race {
+  private static _createdInstances = 0;
+  private readonly _maxLifePoints = 80;
   constructor(
     _name: string,
     _dexterity: number,
-    private readonly _maxLifePoints: number,
+    
   ) {
     super(_name, _dexterity);
+    
+    Dwarf._createdInstances += 1; 
+  }
+
+  static createdRacesInstances(): number {
+    return Dwarf._createdInstances;
   }
 
   get maxLifePoints(): number {
--- src/Races/index.ts
+++ src/Races/index.ts
@@ -1,3 +1,10 @@
 import Race from './Race';
+import Dwarf from './Dwarf';
+import Elf from './Elf';
 
 export default Race;
+
+export {
+  Dwarf,
+  Elf,
+};
