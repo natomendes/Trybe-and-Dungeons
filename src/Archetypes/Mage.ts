f17db8f586e53179128a5ba17e11888d3b52d50e
Renato Mendes <mendeslrenato@gmail.com>
2022-11-18 20:02:20 -0300

    feat: ensure Mage class returns the right special when value is passed to constructor

--- src/Archetypes/Mage.ts
+++ src/Archetypes/Mage.ts
@@ -2,11 +2,15 @@ import { EnergyType } from '../Energy';
 import Archetype from './Archetype';
 
 export default class Mage extends Archetype {
+  private static createdInstances = 0;
   constructor(
     name: string,
     private readonly _energyType: EnergyType,
+    special?: number,
   ) {
-    super(name);
+    super(name, special || 0);
+
+    Mage.createdInstances += 1;
   }
 
   get energyType(): EnergyType {
