5bba37cdb9b111510dcb4bcc9c64390c3063a4e8
Renato Mendes <mendeslrenato@gmail.com>
2022-11-20 18:19:36 -0300

    test: ensure Dragon has the right name

--- src/tests/Dragon.spec.ts
+++ src/tests/Dragon.spec.ts
@@ -8,5 +8,10 @@ describe('Dragon Class', () => {
   it('Should have lifePoints set to 999', () => {
     const sut = makeSut();
     expect(sut.lifePoints).toBe(999);
-  })
+  });
+
+  it('Should have name set to Dragon', () => {
+    const sut = makeSut();
+    expect(sut.name).toBe('Dragon');
+  });
 })
