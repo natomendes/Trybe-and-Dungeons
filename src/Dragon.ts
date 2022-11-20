3b96be97e0eb33fc947d1632466fd261a7859fc4
Renato Mendes <mendeslrenato@gmail.com>
2022-11-20 18:19:18 -0300

    feat: ensure Dragon has the right name

--- src/Dragon.ts
+++ src/Dragon.ts
@@ -1,7 +1,13 @@
 import Monster from './Monster';
 
 export default class Dragon extends Monster {
-  constructor() {
+  constructor(
+    private _name: string = 'Dragon',
+  ) {
     super(999);
+  }
+
+  get name(): string {
+    return this._name;
   }
 }
