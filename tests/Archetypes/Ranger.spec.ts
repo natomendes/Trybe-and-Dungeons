7000ea732afce6823673fa61c85b3019859f2793
Renato Mendes <mendeslrenato@gmail.com>
2022-11-20 19:38:26 -0300

    test: ensure Ranger has static method createdArchetypeInstances

--- tests/Archetypes/Ranger.spec.ts
+++ tests/Archetypes/Ranger.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 import Archetype, { Ranger } from '../../src/Archetypes';
 
 const makeSut = (): Archetype => {
@@ -36,5 +37,11 @@ describe('Ranger Class', function () {
   it('Should return the right cost when value is provided to constructor', function () {
     const sut = makeSutWithCostValue();
     expect(sut.cost).toBe(70);
+  });
+
+  it('Should have createdRacesInstances static method', function () {
+    const createdArchetypeSpy = jest.spyOn(Ranger, 'createdArchetypeInstances');
+    Ranger.createdArchetypeInstances();
+    expect(createdArchetypeSpy).toHaveBeenCalled();
   });
 });
