aef7a979741664b32f382dafc61256540428d72f
Renato Mendes <mendeslrenato@gmail.com>
2022-11-20 19:19:24 -0300

    fix: fix Orc class test

--- tests/Races/Orc.spec.ts
+++ tests/Races/Orc.spec.ts
@@ -24,6 +24,8 @@ describe('Orc Class', function () {
   });
 
   it('Should have createdRacesInstances static method', function () {
-    expect(Orc.createdRacesInstances).toBeDefined();
+    const createdRacesSpy = jest.spyOn(Orc, 'createdRacesInstances');
+    Orc.createdRacesInstances();
+    expect(createdRacesSpy).toHaveBeenCalled();
   });
 });
